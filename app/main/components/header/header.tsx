c29064c79a49d5aa4bb9427f7664e61d9fa534d7
Jeon-YuNa <108977502+Jeon-YuNa@users.noreply.github.com>
2025-03-26 22:09:15 +0900

    Update header.tsx

--- app/main/components/header/header.tsx
+++ app/main/components/header/header.tsx
@@ -7,10 +7,10 @@ const Header = () => {
     <header className="p-3 fixed top-0 w-full z-[999] bg-white">
       <Navigation />
 
-      <h1 style={{ display: "flex", justifyContent: "center", transform: "scale(1.5)" }}>
+      <h1 style={{ display: "flex", justifyContent: "center"}}>
         <Link href={"/main"} style={{ display: "inline-block" }}>
           <MainLogo
-            link={"https://cdn.imweb.me/thumbnail/20241021/b71f086e49061.png"}
+            link={"https://cdn.imweb.me/thumbnail/20250326/25a59b93d501d.png"}
           />
         </Link>
       </h1>
