afa71458278c87b90fd65178b618496e50dbded4
Jeon-YuNa <108977502+Jeon-YuNa@users.noreply.github.com>
2025-06-10 21:03:00 +0900

    Update page.tsx

--- app/detail/[code]/page.tsx
+++ app/detail/[code]/page.tsx
@@ -7,8 +7,8 @@ import "../css/media.css";
 const DetailCodePage = ({ params }: { params: Promise<{ code: string }> }) => {
   const { code } = use(params);
   const searchParams = useSearchParams();
-  const hex = searchParams.get("hex"); // 쿼리 파라미터에서 hex 값을 가져옴
-  console.log(code, hex); // code와 hex 출력
+  const hex = searchParams.get("hex"); 
+  console.log(code, hex); 
 
   return (
     <>
